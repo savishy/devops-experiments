d5dd368dced15c152ae7fa6296e828bba5fff463
Vish <savishy.pv@gmail.com>
2017-02-02 06:41:21 +0000

    #17 WIP

--- jenkins/job-config/common.groovy
+++ jenkins/job-config/common.groovy
@@ -1,4 +1,17 @@
 def hello() {
   echo "hello"
 }
+
+/**
+**/
+def checkoutGit(repoUrl) {
+  git url: ${repoUrl}
+}
+
+/**
+
+**/
+def buildWar() {
+  sh "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 && ./mvnw install"
+}
 return this
