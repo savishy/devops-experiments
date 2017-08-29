461cd2c64aec8bdacda8e49a5aebf4391679d6ad
Vish <savishy.pv@gmail.com>
2017-08-29 13:37:44 +0530

    #20 jenkinsfile improvements WIP - builds docker image

--- jenkins/job-config/common.groovy
+++ jenkins/job-config/common.groovy
@@ -16,4 +16,28 @@ def buildWar(javaHome,buildCmd) {
   sh "export JAVA_HOME=${javaHome} && ${buildCmd}"
 }
 
+/**
+BUILD DOCKER IMAGE
+
+Parameters:
+- dockerImageName: name with tags of docker image to create.
+- dockerWorkspace: relative directory containing a Dockerfile.
+- dockerServer: URI for a Docker Daemon Default: tcp://127.0.0.1:2375
+**/
+def buildDockerImage(
+  dockerImageName,
+  dockerWorkspace,
+  dockerServer = "tcp://127.0.0.1:2375") {
+
+  docker.withServer(dockerServer) {
+    docker.build(dockerImageName,dockerWorkspace)
+  }
+
+}
+
+
+def getMavenProjectVersion() {
+  sh "mvn -o -q -Dexec.executable='echo' -Dexec.args='${project.version}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec"
+}
+
 return this
