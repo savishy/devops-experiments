abdf1fd468411a9f3d8ef998c620f56a039f9d29
Vish <savishy.pv@gmail.com>
2017-08-29 14:21:23 +0530

    #20 WIP

--- jenkins/job-config/common.groovy
+++ jenkins/job-config/common.groovy
@@ -42,7 +42,10 @@ http://mrhaki.blogspot.in/2009/04/escaping-quotes-in-groovy-strings.html
 https://groups.google.com/forum/#!topic/groovy-user/PAjWvGeu1rs
 **/
 def getMavenProjectVersion() {
-  sh "mvn -o -q -Dexec.executable='echo' -Dexec.args='${project.version}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec"
+
+  def cmd = $/./mvnw -q -Dexec.executable="echo" -Dexec.args='$${project.version}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec/$
+  def retVal = sh returnStdOut: true, script: cmd
+  return retVal
 }
 
 return this
